6c10f876876ee96a32639125e4477bf43c43fb75
Abdul Ahad Rauf <120673653+AbdulAhadRauf@users.noreply.github.com>
2023-02-23 12:30:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+//          // //QUESTION 1 // //
+//class Main{
+//   public static void main(String args[]) {
+//     int a, b;
+//     a = 10;
+//     b= 20;
+//     System.out.println(a+b);
+//   }
+// }
+
+//             // // ##QUESTION 2
+// class Student{
+//   String rollno, name ;
+//   // public Student(){}
+//   public Student(String n, String rn){
+//     name = n;
+//     rollno = rn;
+//   }
+// }
+// class Main{
+//   public static void main(String[] args){
+//     Student s = new Student("aadi", "1");
+//     System.out.println(s.name +" "+ s.rollno);
+//   }
+// }
